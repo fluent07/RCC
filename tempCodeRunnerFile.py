55eba374b350cbb5e4d88e5e251083b9620274f8
Vinod Akurati <vinod.akurati4@gmail.com>
2025-03-21 17:54:52 +0100

    deleted redundant files

--- tempCodeRunnerFile.py
+++
@@ -1,8 +0,0 @@
-for key, value in results.items():
-#     print(f"{key}: {value}")
-# print("\n\n")
-
-# # Print dictionary with colors
-# for key, value in results.items():
-#     print(f"{colors.get(key, Fore.WHITE)}{key}: {value}{Style.RESET_ALL}")
-# print("\n\n")
